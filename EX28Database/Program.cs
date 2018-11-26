f66de0c80a971a792cc8152cc35f0932b58c3116
Kasper Bjørnstrup Andersen <kasp723s@edu.eal.dk>
2018-11-26 10:53:55 +0100

    Add project files.

---
+++ EX28Database/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EX28Database
+{
+    class Program
+    {
+        private static string connectionString = "Server =[server_name];Database =[database_name];Trusted_Connection = True";
+        static void Main(string[] args)
+        {
+            Program prog = new EX28Database.Program();
+            prog.Run();
+        }
+        private void Run()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("InsertPet", con);
+                    cmd1.CommandType = System.Data.CommandType.StoredProcedure;
+                }
+               
+            }
+                    
+                }
+
+    }
+}
